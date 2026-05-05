a1ef9b64bf016bf42a1ff37e69124d017780f8c5
Ali Hassan <alihassandevnext@gmail.com>
2026-05-05 12:48:15 +0500

    Create Django Proxy Services of Backend APIs

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -13,10 +13,13 @@ urlpatterns = [
 
     # path for logout
     path(route='logout', view=views.logout_request, name='logout'),
-
-    # path for dealer reviews view (to be done in next phases)
-
-    # path for add a review view (to be done in next phases)
+    
+    # get dealerships and dealer details
+    path(route='get_dealers', view=views.get_dealerships, name='get_dealers'),
+    path(route='get_dealers/<str:state>', view=views.get_dealerships, name='get_dealers_by_state'),
+    path(route='dealer/<int:dealer_id>', view=views.get_dealer_details, name='dealer_details'),
+    path(route='reviews/dealer/<int:dealer_id>', view=views.get_dealer_reviews, name='dealer_details'),
+    path(route='add_review', view=views.add_review, name='add_review'),
 
     # path for cars
     path(route='get_cars', view=views.get_cars, name ='getcars'),
